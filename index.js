11a9b29c9037c2f31a57f77c203e2c5f7aa1c750
Golla Pradeep Kumar <pradeepubuntuvps@gmail.com>
2025-02-04 14:22:16 +0530

    Add multer for image upload

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ const MONGO_URI="mongodb://localhost:27017/mongoconnect";
 /* this*/
 const express=require("express");
 const mongoose=require("mongoose");
+const { upload } = require("./multer");
 const app=express();
+
+app.post("/upload",upload.single("img"),(req,res)=>{
+    res.status(200).json({success: true,message: "File uploaded successfully."});
+})
+
 mongoose.connect(MONGO_URI).then(()=>{
     app.listen(7777,()=>{
         console.log("App listening on port 7777");
